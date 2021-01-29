278bd01af910238b3e4a50549b1855d34549b82b
otterSp0tter <hep.crossing@gmail.com>
2021-01-29 17:38:50 +1100

    created list builder for credits page

--- lib/services/credits_link_list.dart
+++
@@ -1,9 +0,0 @@
-class LinkList{
-
-  String image;
-  String url;
-  String label;
-
-  LinkList({ this.image, this.url, this.label });
-
-}
--- lib/widgets/credits_button.dart
+++ lib/widgets/credits_button.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:soil_mate/screens/home/credits.dart';
 import 'package:url_launcher/url_launcher.dart';
-import '../services/credits_link_list.dart';
-export '../services/credits_link_list.dart';
 
-class CreditsButton extends StatelessWidget {
-  String image;
-  String url;
-  String label;
+class CreditsButton extends StatefulWidget {
 
-  CreditsButton({
-    Key /*?*/ key,
-    @required this.image,
-    @required this.url,
-    @required this.label,
-  }) : super(key: key) {}
+  @override
+  _CreditsButtonState createState() => _CreditsButtonState();
+}
 
+class _CreditsButtonState extends State<CreditsButton> {
+  int index;
 
   List<LinkList> links = [
     LinkList(url: 'https://discord.gg/8x58DuxfGz', label: 'Join our community on Discord', image: 'discord_logo.jpg'),
@@ -23,52 +18,51 @@ class CreditsButton extends StatelessWidget {
     LinkList(url: 'https://open-source-agriculture.github.io', label: 'Explore our website', image: 'website_logo.png'),
   ];
 
-
-
-
-
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      color: Colors.white,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(18.0),
-          side: BorderSide(color: Colors.grey)),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            label,
-            style: buttonTextStyle(),
-          ),
-          Container(
-            width: 60,
-            height: 60,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                  image: AssetImage(image),
-                  fit: BoxFit.fill
+    return Container(
+      height: 300,
+      child: ListView.builder(
+          itemCount: links.length,
+          itemBuilder: (context, index){
+            return Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(color: Colors.black, width: 1.0),
+                ),
+                child: ListTile(
+                  onTap: () async {
+                    if (await canLaunch(links[index].url)) {
+                      await launch(links[index].url);
+                    } else {
+                      throw 'Could not launch ${links[index].url}';
+                    }
+                  },
+                  title: Text(links[index].label,
+                    style: buttonTextStyle(),
+                  ),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage('assets/${links[index].image}'),
+                    backgroundColor: Colors.white,
+                  ),
+                ),
               ),
-            ),
-          ),
-        ],
+            );
+          }
       ),
-      onPressed: () async {
-        if (await canLaunch(url)) {
-        await launch(url);
-        } else {
-        throw 'Could not launch $url';
-        }
-      },
     );
   }
 }
 
-buttonTextStyle() {
-  return TextStyle(
-    fontFamily: 'Dosis',
-    fontWeight: FontWeight.w900,
-    fontSize: 20,
-  );
+
+class LinkList{
+
+  String image;
+  String url;
+  String label;
+
+  LinkList({ this.image, this.url, this.label });
+
 }
