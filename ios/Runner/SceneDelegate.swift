34dc667900fa5cebedfa0021b5b5b64a821613ee
Kipling <kip.crossing@gmail.com>
2021-02-22 13:51:24 +1100

    prep for app store 3

---
+++ ios/Runner/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.swift
+//  Runner
+//
+//  Created by user on 22/2/21.
+//
+
+import Foundation
+
+import UIKit
+
+@available(iOS 13.0, *)
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+//...
+}
