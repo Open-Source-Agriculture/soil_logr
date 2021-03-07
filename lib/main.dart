8cdf716f69332eedc6727dfa7bd08cc2946a87e4
kipling <kip.crossing@gmail.com>
2021-03-07 18:39:23 +1100

    Limit user to only texture survey while other one is WIP

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:soil_mate/screens/sample_list.dart';
 import 'package:soil_mate/screens/side_bar/sidebar_layout.dart';
 
 void main() => runApp(MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
           scaffoldBackgroundColor: Colors.white,
           primaryColor: Colors.white
       ),
-      home: SideBarLayout(),
+      home: SampleList(),
     );
   }
 }
