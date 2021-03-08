d97f984dfa76d2553885e5cd3991bcd39afd7503
otterSp0tter <hep.crossing@gmail.com>
2021-03-08 12:37:01 +1100

    imported hive

--- lib/models/log.dart
+++ lib/models/log.dart
@@ -1,49 +1,52 @@
+import 'package:hive/hive.dart';
 
 
+part 'log.g.dart';
 
-
+@HiveType(typeId: 1)
 class GeoField{
+  @HiveField(0)
   final double lat;
+  @HiveField(1)
   final double lon;
 
   GeoField({this.lat, this.lon});
 
-  Map<String, dynamic> toMap() {
-    return {
-      'lat': lat,
-      'lon': lon,
-    };
-  }
+
 }
 
 
-
+@HiveType(typeId: 2)
 class Quantity{
+  @HiveField(0)
   final String measure;
+  @HiveField(1)
   final double value;
+  @HiveField(2)
   final String label;
 
   Quantity({this.measure, this.value, this.label});
 
-  Map<String, dynamic> toMap() {
-    return {
-      'measure': measure,
-      'value': value,
-      'label': label
-    };
-  }
 }
 
-
+@HiveType(typeId: 3)
 class Log{
 
+  @HiveField(0)
   List<Quantity> quantity;
+  @HiveField(1)
   int id;
+  @HiveField(2)
   String name;
+  @HiveField(3)
   String type; // "farm_soil_test"
+  @HiveField(4)
   String timestamp;
+  @HiveField(5)
   List<Map> log_category;  // from taxonomies
+  @HiveField(6)
   String notes;
+  @HiveField(7)
   GeoField geofield; // with keys lat lon
 
 //  int depthShallow;
