7ce82edba02a5c78cff238934a5990edf78f043b
otterSp0tter <hep.crossing@gmail.com>
2021-03-12 15:44:05 +1100

    home page now scrollable. swapped an icon

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -29,49 +29,60 @@ class _HomeState extends State<Home> {
         elevation: 2.0,
         actions: <Widget>[],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  children: [
-                    Text('Welcome to the ' + ' \n Soil LogR App!',
-                      style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.w600,
-                        fontSize: displayWidth(context) * 0.08,
+      body: CustomScrollView(
+        shrinkWrap: true,
+          slivers: <Widget>[
+            SliverPadding(
+            padding: const EdgeInsets.all(16.0),
+              sliver: SliverList(
+                delegate: SliverChildListDelegate(
+                  <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(height: displayHeight(context)*0.08,),
+                      Text('Welcome to the ' + ' \n Soil LogR App!',
+                        style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w600,
+                          fontSize: displayWidth(context) * 0.08,
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 20,),
-                    Text('To get started, choose one of our surveys', style: bodyTextStyle(context),),
-                    Text('by using the top left menu', style: bodyTextStyle(context),),
-                    Container(
-                      color: Colors.grey.withOpacity(0.3),
-                        child: Icon(Icons.menu)),
-                  ],
-                ),
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/soil_mate_logo.png'),
-                  radius: displayWidth(context)*0.1,
-                ),
-                Column(
-                  children: [
-                    Text('What is Soil LogR?', style: headingTextStyle(context),),
-                    Text('The Soil LogR App is a tool to aid on site ', style: bodyTextStyle(context),),
-                    Text('surveys by ' +
-                        'automatically capturing ', style: bodyTextStyle(context),),
-                    Text('location data', style: bodyTextStyle(context),),
-                  ],
-                )
+                      SizedBox(height: displayHeight(context)*0.04,),
+                      Text('To get started, choose one of our surveys', style: bodyTextStyle(context),),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('by using the top left menu', style: bodyTextStyle(context),),
+                          RotationTransition(
+                            turns: AlwaysStoppedAnimation(200 / 360),
+                              child: Icon(Icons.subdirectory_arrow_right)),
+                        ],
+                      ),
+                    ],
+                  ),
+                    SizedBox(height: displayHeight(context)*0.04,),
+                  CircleAvatar(
+                    backgroundImage: AssetImage('assets/soil_mate_logo.png'),
+                    radius: displayWidth(context)*0.1,
+                  ),
+                    SizedBox(height: displayHeight(context)*0.04,),
+                  Column(
+                    children: [
+                      Text('What is Soil LogR?', style: headingTextStyle(context),),
+                      SizedBox(height: displayHeight(context)*0.02,),
+                      Text('The Soil LogR App is a tool to aid on site ', style: bodyTextStyle(context),),
+                      Text('surveys by ' +
+                          'automatically capturing ', style: bodyTextStyle(context),),
+                      Text('location data', style: bodyTextStyle(context),),
+                    ],
+                  )
 
-              ],
+                ],
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
