d930c5556e61dabebfa23732ecc8e357471cc565
otterSp0tter <hep.crossing@gmail.com>
2021-03-12 15:24:23 +1100

    minor UI changes

--- lib/widgets/sample_list_tile.dart
+++ lib/widgets/sample_list_tile.dart
@@ -25,29 +25,32 @@ class SampleListTile extends StatelessWidget {
       }
     });
 
-    return Container(
-      decoration: BoxDecoration(
-        //borderRadius: BorderRadius.circular(15),
-        //border: Border.all(color: color, width: 3.0),
-        color: color.withOpacity(0.4),
-      ),
-      child: Slidable(
-        actionPane: SlidableScrollActionPane(),
-        actions: [
-          IconSlideAction(
-            caption: 'Cancel',
-            color: Colors.grey[300],
-            icon: Icons.cancel_outlined,
-            onTap: (){},
+    return Padding(
+      padding: const EdgeInsets.only(top: 1.0),
+      child: Container(
+        decoration: BoxDecoration(
+          //borderRadius: BorderRadius.circular(15),
+          //border: Border.all(color: color, width: 3.0),
+          color: color.withOpacity(0.5),
+        ),
+        child: Slidable(
+          actionPane: SlidableScrollActionPane(),
+          actions: [
+            IconSlideAction(
+              caption: 'Cancel',
+              color: Colors.grey[300],
+              icon: Icons.cancel_outlined,
+              onTap: (){},
+            ),
+          ],
+          child: ListTile(
+            title: Text('ID: '+ textureLog.id.toString()
+                 + '    ' +textureLog.name
+                + '\n' + textureLog.geofield.lat.toString() + ', '
+                + textureLog.geofield.lon.toString()
+                ),
+            subtitle: Text(quantityString),
           ),
-        ],
-        child: ListTile(
-          title: Text('ID: '+ textureLog.id.toString()
-               + '    ' +textureLog.name
-              + '\n' + textureLog.geofield.lat.toString() + ', '
-              + textureLog.geofield.lon.toString()
-              ),
-          subtitle: Text(quantityString),
         ),
       ),
     );
